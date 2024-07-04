8fd2cfa4347e2272395314c73282f5a459862ed4
runohod <andrei.rumynthev1728@yandex.ru>
2024-07-04 09:30:44 +0300

    init commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,43 +1,38 @@
-//тип данных товара
 export interface IProductItem {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number|null;
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number;
 }
 
-//состояние страницы
-export interface IAppState {
-    catalog: IProductItem[];
-    basket: string[];
-    order: IOrder | null;
+export interface IDeliveryForm {
+	payment: string;
+	address: string;
 }
 
-//данные из формы доставки и метода оплаты
-export interface IAdressForm {
-    payment: string;
-    address: string;
+export interface IContactsForm {
+	email: string;
+	phone: string;
 }
 
-//данные из формы контактов
-export interface IContactsForm  {
-    email: string;
-    phone: string;
+export interface IOrderForm extends IDeliveryForm, IContactsForm {
+	total: number;
+	items: string[];
 }
 
-//данные заказа
-export interface IOrder extends IAdressForm, IContactsForm {
-    items: string[];
-    total: number;
-}
-
-//данные заказа на отправку серверу
 export interface IOrderResult {
-    id: string;
-    total: number;
+	id: string;
+	total: number;
 }
 
-//данные ошибки ввода форм
-export type FormErrors = Partial<Record<keyof IOrder, string>>;
+export interface IAppState {
+	catalog: IProductItem[];
+	basket: IProductItem[];
+	preview: string | null;
+	order: IOrderForm | null;
+	loading: boolean;
+}
+
+export type FormErrors = Partial<Record<keyof IOrderForm, string>>;
